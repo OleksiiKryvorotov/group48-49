6dc9764aeee5e7fbf8719f0d75ab7a31c0d96b0f
alexykr alexykr <sumskaqqq@gmail.comgit config --global user.name Oleksii>
2022-12-12 11:11:43 +0100

    new

--- src/ClassWork/Lesson1.java
+++ src/ClassWork/Lesson1.java
@@ -4,6 +4,14 @@ public class Lesson1 {
     public static void main(String[] args) {
         int a = 1000;
         int b = 500;
+int x = a * b;
+System.out.println(x);
 
+int n = 365;
+int e = n % 10;
+System.out.println(e);
+
+int q = n/10;
+System.out.println(q);
     }
 }
