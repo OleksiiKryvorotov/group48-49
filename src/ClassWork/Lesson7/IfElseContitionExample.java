29cf598289d1d5b542b219c70aeea3a3e758ff2a
alexykr alexykr <sumskaqqq@gmail.comgit config --global user.name Oleksii>
2022-12-19 11:06:31 +0100

    upd

--- src/ClassWork/Lesson7/IfElseContitionExample.java
+++ src/ClassWork/Lesson7/IfElseContitionExample.java
@@ -3,7 +3,7 @@ package ClassWork.Lesson7;
 public class IfElseContitionExample {
     public static void main(String[] args) {
 
-        int x=100;
+        /*int x=100;
         int y=0;
 
         if (x<100) {
@@ -13,6 +13,30 @@ public class IfElseContitionExample {
         } else {
              y = x*x;
         }
-        System.out.println(y);
+        System.out.println(y);*/
+        int x = 100;
+        int z;
+        int a;
+
+        if (x < 100 && (x % 2 == 0))
+        //вычисляем знач. выражения (x<100) - результат или true или false
+        //вычисляем знач. выражения (x % 2 ==0) - результат или true или false
+        //для этого выражения сперва вычмсл. остаток от деления ч % 2
+        // и далее сравнивается этот результат с 0
+        //
+        //
+        //
+        //
+        {
+            int y = x * 25;
+            z = y * y;
+            a = z * 10;
+
+        } else if (x > 100) {
+            int y = x / 5;
+            z = y * y;
+            a = z * 10;
+
+        }
     }
 }
